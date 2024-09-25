46300c1f4744726c0b67b972615645d270834b87
dbwcooper <dbwencookie@gmail.com>
2024-09-25 22:15:45 +0800

    fix: tailwindcss config

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { router } from './router'
+import App from './App'
+import './index.css'
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <App />
     </StrictMode>,
   )
 }
